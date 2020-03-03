e1466e99a62881e3814e6f21c6b1dd445c732535
mameiying <mameiying@beisen.com>
2020-03-03 21:59:03 +0800

    feat: gulp配置，导出commonjs包

--- types/test/index.d.ts
+++
@@ -1,6 +0,0 @@
-/**
- * 组件入口文件
- */
-import Test from './test';
-export default Test;
-export * from './interface';
--- types/test/interface.d.ts
+++
@@ -1,6 +0,0 @@
-/**
- * 组件类型声明文件
- */
-export interface ITestProps {
-    name: string;
-}
