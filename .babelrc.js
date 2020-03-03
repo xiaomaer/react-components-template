14d1e58568222c890a9ccf96774b33f4f73fd5e1
mameiying <mameiying@beisen.com>
2020-03-03 22:03:09 +0800

    feat: 修改babel配置和gulp配置，导出es module包

--- .babelrc.js
+++ .babelrc.js
@@ -1,4 +1,26 @@
 module.exports = {
   presets: ['@babel/env', '@babel/typescript', '@babel/react'],
   plugins: ['@babel/plugin-transform-runtime', '@babel/proposal-class-properties'],
+  env: {
+    esm: {
+      presets: [
+        [
+          '@babel/env',
+          {
+              // 导出 ES module
+            modules: false,
+          },
+        ],
+      ],
+      plugins: [
+        [
+          '@babel/plugin-transform-runtime',
+          {
+              // 导出 ES module
+            useESModules: true,
+          },
+        ],
+      ],
+    },
+  },
 };
