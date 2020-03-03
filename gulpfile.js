1dd435e235bbbae069d91f6409c83b1d2d8ce76c
mameiying <mameiying@beisen.com>
2020-03-03 22:23:19 +0800

    feat: 修改gulp配置，在style文件夹下生成css.js文件，实现组件样式按需引入

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ const babel = require('gulp-babel');
 const less = require('gulp-less');
 const autoprefixer = require('gulp-autoprefixer');
 const cssnano = require('gulp-cssnano');
+const through2 = require('through2');
 
 const paths = {
   dest: {
@@ -15,17 +16,44 @@ const paths = {
 };
 
 /**
+ * 当前组件样式 import './index.less' => import './index.css'
+ * 依赖的其他组件样式 import '../test-comp/style' => import '../test-comp/style/css.js'
+ * 依赖的其他组件样式 import '../test-comp/style/index.js' => import '../test-comp/style/css.js'
+ * @param {string} content
+ */
+function cssInjection(content) {
+  return content
+    .replace(/\/style\/?'/g, "/style/css'")
+    .replace(/\/style\/?"/g, '/style/css"')
+    .replace(/\.less/g, '.css');
+}
+
+/**
  * 编译脚本文件
- * @param {string} babelEnv babel环境变量
- * @param {string} destDir 目标目录
+ * @param {*} babelEnv babel环境变量
+ * @param {*} destDir 目标目录
  */
 function compileScripts(babelEnv, destDir) {
   const { scripts } = paths;
-  // 设置环境变量
   process.env.BABEL_ENV = babelEnv;
   return gulp
     .src(scripts)
     .pipe(babel()) // 使用gulp-babel处理
+    .pipe(
+      through2.obj(function z(file, encoding, next) {
+        this.push(file.clone());
+        // 找到目标(截住style/index.js，生成style/css.js，并通过正则将引入的less文件后缀改成css，从而在style文件夹下生成css.js)
+        if (file.path.match(/(\/|\\)style(\/|\\)index\.js/)) {
+          const content = file.contents.toString(encoding);
+          file.contents = Buffer.from(cssInjection(content)); // 文件内容处理
+          file.path = file.path.replace(/index\.js/, 'css.js'); // 文件重命名
+          this.push(file); // 新增该文件
+          next();
+        } else {
+          next();
+        }
+      }),
+    )
     .pipe(gulp.dest(destDir));
 }
 
